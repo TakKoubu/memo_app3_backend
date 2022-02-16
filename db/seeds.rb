b0d4af2e819a0e648df72575086d45f7cc41b00f
TakKoubu <48621700+TakKoubu@users.noreply.github.com>
2022-02-16 14:46:00 +0900

    memo seed fileの追加 (#2)

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ User.create!(name: 'test2', email: 'test2@gmail.com', password: 'aaaaaa', passwo
 User.create!(name: 'test3', email: 'test3@gmail.com', password: 'aaaaaa', password_confirmation: 'aaaaaa')
 User.create!(name: 'test4', email: 'test4@gmail.com', password: 'aaaaaa', password_confirmation: 'aaaaaa')
 User.create!(name: 'test5', email: 'test5@gmail.com', password: 'aaaaaa', password_confirmation: 'aaaaaa')
+
+Memo.create!(user_id: 1, content: 'あいうえお')
+Memo.create!(user_id: 2, content: 'かきくけこ')
+Memo.create!(user_id: 3, content: 'さしすせそ')
+Memo.create!(user_id: 4, content: 'たちつてと')
+Memo.create!(user_id: 5, content: 'なにぬねの')
+
+

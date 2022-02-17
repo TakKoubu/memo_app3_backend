6ad34259444870bb0b5eb15237429189ea391a13
TakKoubu <48621700+TakKoubu@users.noreply.github.com>
2022-02-17 09:36:13 +0900

    memoの新規作成 (#4)

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -3,5 +3,19 @@ module Api
     def index
       render json: Memo.all
     end
+
+    def create
+      memo = current_user.memos.build(memo_params)
+      if memo.save
+        render json: memo
+      else
+        render json: memo.errors, status: 422
+      end
+    end
+
+    private 
+      def memo_params
+        params.require(:memo).permit(:content)
+      end
   end
 end
